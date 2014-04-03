646df1c13b813ea431f7cd37a0a079d13bf2a0e7
Tao Lei <taolei@rosetta2.csail.mit.edu>
2014-04-03 14:18:54 -0400

    mc

--- run.slient.sh
+++ run.slient.sh
@@ -11,7 +11,7 @@ shift
  ln -s $args.train.$type data/$args.train.$type.$runid
  ln -s $args.test.$type data/$args.test.$type.$runid
 
-	java -classpath "bin:lib/trove.jar" -Xmx20000m parser.DependencyParser model-file:runs/$args.model.$type.$runid train train-file:data/$args.train.$type.$runid unimap-file:unimap/$args.uni.map test test-file:data/$args.test.$type.$runid $@ | tee runs/$args.$type.$runid.log
+	java -classpath "bin:lib/trove.jar" -Xmx30000m parser.DependencyParser model-file:runs/$args.model.$type.$runid train train-file:data/$args.train.$type.$runid unimap-file:unimap/$args.uni.map test test-file:data/$args.test.$type.$runid $@ > runs/$args.$type.$runid.log
 
     rm data/$args.train.$type.$runid
     rm data/$args.test.$type.$runid
