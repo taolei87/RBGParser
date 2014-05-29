5a766c6c528ab32c6fd21038f07c87f6081867e1
Tao Lei <taolei@rosetta1.csail.mit.edu>
2014-05-29 14:42:03 -0400

    mc

--- len_uas.py
+++ len_uas.py
@@ -3,17 +3,17 @@ import os, sys;
 
 def main():
     lst = []
-    print os.sys[1];
-    with open(os.sys[1]) as fin:
+    print sys.argv[1]
+    with open(sys.argv[1]) as fin:
         for line in fin:
             parts = line.split();
             if len(parts) != 3: continue;
-            t = [ int(x) for x in parts if x.isdigit ];
+            t = [ int(x) for x in parts if x.isdigit() ];
             if len(t) != 3: continue;
             lst.append(t);
     print len(lst);
     lst = sorted(lst);
-    n = 10;
+    n = 5;
     m = len(lst)/n;
     st = 0;
     ed = 0;
@@ -24,6 +24,13 @@ def main():
             ed = ed + 1;
         print st, ed;
         lst2.append(lst[st:ed]);
+        st = ed;
+
+    for seg in lst2:
+        nlen = sum(x[0] for x in seg) + 0.0;
+        ua = sum(x[1] for x in seg) + 0.0;
+        ntok = sum(x[2] for x in seg) + 0.0;
+        print "%.1f\t%.2f" % (nlen/len(seg), ua/ntok*100);
 
 if __name__ == "__main__":
     main();
