d4e3fe3f0c17b24c1f12806fa08f69f9333669a4
Tao Lei <taolei@rosetta1.csail.mit.edu>
2014-05-29 15:10:40 -0400

    mc

--- len_uas.py
+++ len_uas.py
@@ -10,6 +10,9 @@ def main():
             if len(parts) != 3: continue;
             t = [ int(x) for x in parts if x.isdigit() ];
             if len(t) != 3: continue;
+            tt = t[1];
+            t[1] = t[2];
+            t[2] = tt;
             lst.append(t);
     print len(lst);
     lst = sorted(lst);
@@ -28,8 +31,8 @@ def main():
 
     for seg in lst2:
         nlen = sum(x[0] for x in seg) + 0.0;
-        ua = sum(x[1] for x in seg) + 0.0;
-        ntok = sum(x[2] for x in seg) + 0.0;
+        ua = sum(x[2] for x in seg) + 0.0;
+        ntok = sum(x[1] for x in seg) + 0.0;
         print "%.1f\t%.2f" % (nlen/len(seg), ua/ntok*100);
 
 if __name__ == "__main__":
